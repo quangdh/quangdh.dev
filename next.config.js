969cfaa9c30d49e0a95733113a27fd0569b31336
Huy Quang Do <quangdh@vn-sis.com>
2020-07-17 22:50:34 +0700

    frist commit

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+
+const plugins = [
+  [withCSS, {}],
+  [withSass, {}],
+];
+
+const nextConfiguration = {
+  target: "serverless",
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  //   exportPathMap: function () {
+  //     return {
+  //       "/": { page: "/home" },
+  //       "/privacy_policy.html": { page: "/policy" },
+  //     };
+  //   },
+};
+
+module.exports = withPlugins([...plugins], nextConfiguration);
